a94cc4b64a19fa559163c4eea513874c8600c1de
BaoqingXie <bxie4@ucsc.edu>
2020-05-09 18:06:38 -0700

    delete files

--- src/main.js
+++ src/main.js
@@ -1,19 +1,9 @@
 
 let config = {
     type: Phaser.CANVAS,
-    width: 640,
-    height: 480,
+    width: 800,
+    height: 600,
     scene: [Menu, Play],
 };
 
 let game = new Phaser.Game(config);
-
-// define game settings
-game.settings = {
-    spaceshipSpeed: 3,
-    gameTimer: 60000    
-}
-
-//reserve some keyboard varibles
-
-let keyF, keyLeft, keyRight;
