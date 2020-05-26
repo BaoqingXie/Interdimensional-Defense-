2a873b517815fd33194619d72e717390f4dea557
azplayer9 <az94587@gmail.com>
2020-05-25 19:03:39 -0700

    Added enemy types

---
+++ src/prefabs/Charger.js
@@ -0,0 +1,26 @@
+class Charger extends Phaser.GameObjects.Sprite {
+
+    constructor (scene, x, y, texture, frame, dim, spd) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this);
+        
+        scene.physics.world.enable(this);
+        scene.add.existing(this); 
+        this.dimension = dim; // give enemy a dimension it belongs to
+        this.speed = spd;
+        this.hp = 3;
+        this.rotation = Math.PI/2; // face down
+    }
+
+    update() {
+        // chargers run straight downwards (towards the wall)
+        if(this.y < 400) {// WHATEVER PIXEL THE WALL IS AT
+            this.y += this.speed;
+        }
+        else{
+            // attack animation? 
+        } 
+                
+    }
+    
+}
